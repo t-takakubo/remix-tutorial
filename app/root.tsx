36c981d48f0263d08b1422b09b37afebe7603fec
Toshiki Takakubo <t.takakubo.tech@gmail.com>
2025-05-06 16:05:34 +0900

    refactor: migrate from ESLint to Biome for linting and formatting

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,11 @@
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
 
@@ -14,12 +14,12 @@ export const links: LinksFunction = () => [
   {
     rel: "preconnect",
     href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
+    crossOrigin: "anonymous"
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+  }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
